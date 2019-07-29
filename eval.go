fce1d9d3d3ab97badbf5e4050dfbc5db5ffddd8a
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-07-29 09:48:50 -0400

    start constants

--- eval.go
+++ eval.go
@@ -3,30 +3,32 @@ package main
 import (
 	"fmt"
 	"strings"
+	"os"
 
 	"github.com/logrusorgru/aurora"
 )
 
-
 // ReadLine ...
 type ReadLine string
+
+// NewReadLine ...
+func NewReadLine(txt string, err error) ReadLine {
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Error reading input; %s", aurora.Red(err.Error())))
+		os.Exit(1)
+	}
+	return ReadLine(strings.Replace(txt, "\n", "", -1))
+}
 
 func (r ReadLine) toString() string {
 	return string(r)
 }
 
 // Evaluate ...
-func Evaluate(txt ReadLine) {
-	if txt.toString() == "" {
-		fmt.Println(aurora.Magenta("nil"))
-	}
-	parseExpression(txt)
-}
-
-func parseExpression(txt ReadLine) string {
+func Evaluate(txt ReadLine) string {
 	args := strings.Split(txt.toString(), " ")
 
-	switch args[0]{
+	switch args[0] {
 	case "func":
 		// var nameAndArgs string
 		// var returnType string
@@ -47,14 +49,19 @@ func parseExpression(txt ReadLine) string {
 		return "assign"
 	case "const":
 		// read assignment and save
+		AssignC(args[1], args[2:])
 		return "assignC"
+	case "import":
+		return "import"
+	case "pacakge":
+		return "package"
 	default:
-		return "assign"
+		fmt.Println(aurora.Red(fmt.Sprintf("Unknown reference %s", args[0])))
+		return ""
 	}
 
 	return ""
 }
-
 
 func evalFunction(txt ReadLine) {
 	//
@@ -66,15 +73,4 @@ func evalIf(txt ReadLine) {
 
 func evalInt(txt ReadLine) {
 	//
-}
-
-func shouldExit(txt ReadLine) bool {
-	if strings.Compare("exit", txt.toString()) == 0 {
-		return true
-	}
-	return false
-}
-
-func replace(txt string) ReadLine {
-	return ReadLine(strings.Replace(txt, "\n", "", -1))
 }
--- main.go
+++ main.go
@@ -7,32 +7,62 @@ import (
 	"io"
 	"os"
 	"runtime"
+	"strings"
 
 	"github.com/logrusorgru/aurora"
 )
 
+var (
+	variables map[string]interface{}
+	// constants:
+	// c := map[string]map[string]string{
+	//      "test": map[string]string{
+	//            "value": "hello",
+	//            "type": "string",
+	//      }
+	// }
+	constants map[string]interface{}
+	functions map[string]func()
+	types     []string
+)
 
 func init() {
 	printInfo()
 	variables = make(map[string]interface{})
 	constants = make(map[string]interface{})
 	functions = make(map[string]func())
-	types = make(map[string]bool)
+	types = []string{"bool", "byte", "complex128", "complex64", "error", "float32", "float64", "int", "int16", "int32", "int64", "int8", "rune", "string", "uint", "uint16", "uint32", "uint64", "uint8", "uintptr"}
 }
 
 func main() {
 	reader := reader(os.Stdin)
 	loops := 1
-loop:
+app:
 	for {
 		fmt.Print(fmt.Sprintf("go(%v)> ", loops))
-		txt, _ := reader.ReadString('\n')
-		rTxt := replace(txt)
-		if shouldExit(rTxt) {
-			break loop
+		rTxt := NewReadLine(reader.ReadString('\n'))
+
+		if shouldExit(rTxt.toString()) {
+			break app
 		}
-		loops++
+
+		if rTxt.toString() == "" {
+			fmt.Println(aurora.Magenta("nil"))
+			continue app
+		}
+
+		if storedVar := checkVar(rTxt.toString()); storedVar != nil {
+			fmt.Println(aurora.BrightGreen(storedVar.(map[string]string)["value"]))
+			continue app
+		}
+
+		if storedConst := checkConst(rTxt.toString()); storedConst != nil {
+			fmt.Println(aurora.BrightGreen(storedConst.(map[string]string)["value"]))
+			continue app
+		}
+
 		Evaluate(rTxt)
+		loops++
 	}
 }
 
@@ -49,4 +79,17 @@ func reader(r io.Reader) *bufio.Reader {
 	return bufio.NewReader(r)
 }
 
+func shouldExit(txt string) bool {
+	if strings.Compare("exit", txt) == 0 {
+		return true
+	}
+	return false
+}
 
+func checkVar(txt string) interface{} {
+	return variables[txt]
+}
+
+func checkConst(txt string) interface{} {
+	return constants[txt]
+}
